5fa1f20886c20f3f9963cc035f8596261264e1a2
Laura Martin <laumartin4@gmail.com>
2018-05-16 15:07:53 +0200

    restaurants exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+6.times do
+  Restaurant.create(
+    name: Faker::Friends.character,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.subscriber_number(10),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
+
+
+
